8228a94b5ffff171b1025b2acac29420ca322241
jonhnuu <kiyoyu@gmail.com>
2022-06-14 12:49:32 +0700

    upload them thu muc template vo github

--- blogm09/chunha/views.py
+++ blogm09/chunha/views.py
@@ -1,9 +1,6 @@
+from django.http import HttpResponse
 from django.template import loader
-from .models import chunha
 
-...
-def lvbbln(request):
-    chunha:chunha.object.all
-   template = loader.get_template('chunha/lvbbln.html')
-  'chunha:chunha'
-     
+def index(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
