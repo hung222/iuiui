79d99d8a511134462d2cceefa2e689ed411945b1
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-14 22:25:31 +0700

    Delete blogm09/chunha directory

--- blogm09/chunha/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import chunha
-admin.site.register(chunha)
--- blogm09/chunha/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-class chunha(models.Model):
-        tenhoap = models.CharField(max_length=300)
-        congdung = models.CharField(max_length=600)
