08efb8327da8fa6e4f897539d000537cd69679c0
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-14 13:42:36 +0700

    Update views.py

--- blogm09/chunha/views.py
+++ blogm09/chunha/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import chunhas
 
 def index(request):
+  chunha = chunha.objects.filter(firstname='Emil').values()
   template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  context = {
+    'mychunhas': mychunhas,
+  }
+  return HttpResponse(template.render(context, request))
